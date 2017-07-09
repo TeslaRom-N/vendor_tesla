76b0b11591fbd0e51c2245592653a5c1699b06b6
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2017-07-10 01:34:50 +0530

    Make SuperSU optional w/ Magisk[1/2]

--- config/common.mk
+++ config/common.mk
@@ -137,10 +137,12 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     org.dirtyunicorns.utils
 
+ifeq ($(DEFAULT_ROOT_METHOD),supersu)
 # SuperSU
 PRODUCT_COPY_FILES += \
   vendor/tesla/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
    vendor/tesla/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+endif
 
 # Adaway
 PRODUCT_COPY_FILES += \
